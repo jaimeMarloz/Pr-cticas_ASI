cadca21591bc34b769181afefab20f1a9b664685
Jaime <jaimemarloz@usal.es>
2026-09-23 23:07:08 +0200

    Abrir el Firewall
    Se ha añadido una linea al provisioning para que el firewall quede abierto

--- Final/provisioning.sh
+++ Final/provisioning.sh
@@ -6,3 +6,6 @@ apt-get upgrade -y
 curl -fsSL https://get.docker.com | sh
 # instalar Docker Compose
 apt-get install -y docker-compose
+# Habilitar que cual trafico pueda entrar o salir por la interfaz de red
+# Para que el anfitrión pueda acceder al servidor Nginx
+sudo ufw allow in on enp0s8
