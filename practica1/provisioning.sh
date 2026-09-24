de8c7c65fcf3cee4955298bccc21f5d265117d99
Kur0n3-112 <juanriegovila12@gmail.com>
2026-09-24 19:52:03 +0200

    update(24septiembre2026): Cambiado el provisioning.sh para quitar redundancia.

--- practica1/provisioning.sh
+++ practica1/provisioning.sh
@@ -6,9 +6,16 @@ apt-get upgrade -y
 curl -fsSL https://get.docker.com | sh
 
 # instalar Docker Compose
-apt-get update && apt-get upgrade -y && apt-get install -y docker-compose
+apt-get install -y docker-compose
+
 # Habilitar que cual trafico pueda entrar o salir por la interfaz de red
 # Para que el anfitrión pueda acceder al servidor Nginx
 sudo ufw allow in on enp0s8
+
+# Cambia a la direccion donde se encuentran los archivos que se encuentran en
+#   el proyecto (desde donde se hace vagrant up), se encuentran ahi mediante
+#   una carpeta compartida
 cd /vagrant
+
+# Levanta el nginx definido en docker-compose.yml
 docker compose up -d
