4a21994f41a7f48870e7d23711d88868f90add6c
Kur0n3-112 <juanriegovila12@gmail.com>
2026-09-24 19:23:26 +0200

    update(24septiembre2026): Eliminado el archivo texto.txt, cambiado el archivo provishioning.sh para que sea todo mas automatico y annadido un archivo html para que lo muestre nginx.

--- practica1/provisioning.sh
+++ practica1/provisioning.sh
@@ -4,8 +4,11 @@ apt-get update
 apt-get upgrade -y
 # instalar Docker
 curl -fsSL https://get.docker.com | sh
+
 # instalar Docker Compose
-apt-get install -y docker-compose
+apt-get update && apt-get upgrade -y && apt-get install -y docker-compose
 # Habilitar que cual trafico pueda entrar o salir por la interfaz de red
 # Para que el anfitrión pueda acceder al servidor Nginx
 sudo ufw allow in on enp0s8
+cd /vagrant
+docker compose up -d
