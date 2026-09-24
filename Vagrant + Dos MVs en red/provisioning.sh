37c5a0fa8b85ddf3235584355b110f4995330dc1
Kur0n3-112 <juanriegovila12@gmail.com>
2026-09-24 17:54:14 +0200

    merge(24septiembre2026): Juntado todo en una carpeta y eliminado los archivos que sobraban.

--- Vagrant + Dos MVs en red/provisioning.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-apt-get update
-apt-get upgrade -y
-# instalar Docker
-curl -fsSL https://get.docker.com | sh
-# instalar Docker Compose
-apt-get install -y docker-compose
